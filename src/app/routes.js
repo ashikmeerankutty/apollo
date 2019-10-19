6950a91b6b1ce568aee41b8a91b5cb6eb4f754a4
Ashik Meerankutty <ashik9591@gmail.com>
2019-10-19 23:06:16 +0530

    app: updated routes to /maps

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
+import MapsPage from "./pages/MapsPage";
 import App from "./components/App";
 
 class Routes extends Component {
-
   render() {
     return (
       <Router>
         <App>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/maps" component={MapsPage} />
         </App>
       </Router>
     );
