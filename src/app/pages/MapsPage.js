4972bc978f55a21cb56c5717a85ce6e9e07a68c3
Ashik Meerankutty <ashik9591@gmail.com>
2019-10-19 23:07:16 +0530

    app: added a default map

--- src/app/pages/MapsPage.js
+++ src/app/pages/MapsPage.js
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button, Icon } from "antd";
+import ReactMapGL, { GeolocateControl, Marker, Popup } from "react-map-gl";
+import axios from "axios";
 
-const AboutPage = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
-);
+class MapsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 12.972442,
+        longitude: 77.580643,
+        zoom: 15.5,
+        bearing: 0,
+        pitch: 0
+      },
+      mapStyle: "mapbox://styles/mapbox/streets-v11",
+      tags: [],
+      users: [],
+      selectedPerson: null
+    };
+  }
 
-export default AboutPage;
+  async componentDidMount() {
+    try {
+      await this.getTags();
+      await this.getUsers();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  getTags = async () => {
+    let res = await axios.get(`${process.env.REACT_APP_API_URL}/tag`);
+    this.setState({ tags: res.data.tags });
+  };
+
+  getUsers = async () => {
+    let res = await axios.get(`${process.env.REACT_APP_API_URL}/user`);
+    this.setState({ users: res.data.users });
+  };
+
+  _onViewportChange = viewport => this.setState({ viewport });
+
+  _onStyleChange = mapStyle => this.setState({ mapStyle });
+
+  render() {
+    const { viewport, mapStyle, users, tags } = this.state;
+
+    return (
+      <div>
+        <div>
+          {tags.map(tag => (
+            <Button key={tag}>{tag}</Button>
+          ))}
+        </div>
+        <ReactMapGL
+          {...viewport}
+          width="100%"
+          height="500px"
+          mapStyle={mapStyle}
+          onViewportChange={viewport => {
+            this.setState({ viewport });
+          }}
+          mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_KEY}
+        >
+          <GeolocateControl
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+          />
+          <Marker
+            latitude={12.972442}
+            longitude={77.580643}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <Button onClick={(e)=>{
+              e.preventDefault()
+              this.setState({
+                selectedPerson : null
+              })
+            }}>
+              <Icon
+                type="environment"
+                theme="filled"
+                style={{ fontSize: "24px", color: "purple" }}
+              />
+            </Button>
+          </Marker>
+          {/* {selectedPerson ? (Popup)} */}
+        </ReactMapGL>
+      </div>
+    );
+  }
+}
+
+export default MapsPage;
